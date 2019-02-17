c1a96ab12a54ea92d83d0e5a625d984c898b6ffb
Jed <jed324@gmail.com>
2019-02-16 17:03:49 -0800

    ya

--- Main Project/__LOAD.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-::This file was created automatically by CrossIDE to load a hex file using Quartus_stp.
-"C:\altera\15.0\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\Users\jed32\Desktop\School\UBC 2018W2\ReflowOven\Main Project\FSM02123test.HEX" | find /v "Warning (113007)"
--- Main Project/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\danar\Desktop\UBC\ELEC291\Project 1\ReflowOven\Main Project\"
-"D:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\danar\Desktop\UBC\ELEC291\Project 1\ReflowOven\Main Project\FSM0213buzzertests.asm"
-echo Crosside_Action Set_Hex_File C:\Users\danar\Desktop\UBC\ELEC291\Project 1\ReflowOven\Main Project\FSM0213buzzertests.HEX
